9e0d4c1645c11b5ef015480a17bd356dbb527fcb
AbdullahNi <78547088+AbdullahNi@users.noreply.github.com>
2026-05-07 19:57:24 -0400

    Update app.py

--- artifacts/flask-server/app.py
+++ artifacts/flask-server/app.py
@@ -6,6 +6,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+# v2 - updated parser
+
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
 CORS(app, origins="*")
